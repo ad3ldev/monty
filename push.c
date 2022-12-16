3d9ab230497a389564f2c80dd6f67f5648315e7a
Adel ALX <a.adel.a.fattah@gmail.com>
2022-12-16 11:09:19 -0800

    monty compiles

--- push.c
+++ push.c
@@ -16,12 +16,22 @@ void push(stack_t **stack, unsigned int line_cnt)
 	char *n = global.argument;
 
 	if ((is_digit(n)) == 0)
+	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
 		exit(EXIT_FAILURE);
+	}
 	if (global.data_struct == 1)
+	{
 		if (!add_node(stack, atoi(global.argument)))
+		{
 			exit(EXIT_FAILURE);
+		}
+	}
 	else
+	{
 		if (!queue_node(stack, atoi(global.argument)))
+		{
 			exit(EXIT_FAILURE);
+		}
+	}
 }
